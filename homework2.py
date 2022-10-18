6b81daa1d1bd1ebc03b7c06efb36e1f7f33be4f0
NikitaSergeevich <fedorovnikita070@gmail.com>
2022-10-18 23:07:51 +0300

    hm3

--- homework2.py
+++ homework2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import glob
 
-###
+### деление на квартили
 data = pd.read_csv('games/games.csv').sort_values(by='white_rating')
 
 data1 = data[:5015]
@@ -12,7 +12,7 @@ data4 = data[15045:]
 datalist = [data1, data2, data3, data4]
 columns = ['opening_name']
 
-###
+### количество побед
 c = 1
 for i in datalist:
 
@@ -21,20 +21,20 @@ for i in datalist:
 
     fd = open(f'result{c}.csv', 'a')
     my_list = i["white_rating"].values
-    ###
+    ### отброс 20%
     sum_my_list = sum(my_list[int(0.1*len(my_list)):int(0.9*len(my_list))])
     fd.write(f'----------        {int(sum_my_list/len(my_list))}        ----------')
     fd.close()
     c += 1
 
-###
+### запись в файл
 df1 = pd.DataFrame(data['opening_name'].value_counts(), columns=columns)
 df1[:10].to_csv(r'result.csv')
 fd1 = open('result.csv', 'a')
 fd1.write('--------------------------------------------------------------------------')
 fd1.close()
 
-###
+### общий файл
 files = glob.glob('*.csv')
 files.sort()
 combined = pd.DataFrame()
